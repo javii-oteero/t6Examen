26d2511a9141c004f183d8d5ebca4f89e44f6c1f
javi <javii.oteero@gmail.com>
2024-04-16 13:16:48 +0200

    Código mejorado, versión final del proyecto

--- src/es/iesmz/tests/Money.java
+++ src/es/iesmz/tests/Money.java
@@ -7,11 +7,11 @@ public class Money {
     private static final float cambioEurGbp = 0.857839f;
     private static final float cambioGbpEur = 1.165826f;
 
-    private static TipoMoneda[] monedasAceptadas = {TipoMoneda.USD, TipoMoneda.EUR, TipoMoneda.GBP};
+    private static final TipoMoneda[] monedasAceptadas = {TipoMoneda.USD, TipoMoneda.EUR, TipoMoneda.GBP};
 
     public static float change(TipoMoneda origen, TipoMoneda destino, float money){
         if (money < 0) return -1f;
-        else if (!comprobarSiMonedaEstaAceptada(origen) || !comprobarSiMonedaEstaAceptada(destino)) return -1f;
+        else if (comprobarSiMonedaNoEstaAceptada(origen) || comprobarSiMonedaNoEstaAceptada(destino)) return -1f;
         else {
             switch (origen){
                 case GBP -> { return cambiarGbp(destino, money); }
@@ -19,31 +19,31 @@ public class Money {
                 case USD -> { return cambiarUsd(destino, money); }
             }
         }
-        return -1;
+        return -1f;
     }
 
     private static float cambiarUsd(TipoMoneda destino, float money){
         if (destino.equals(TipoMoneda.EUR)) return (money * cambioUsdEur);
         else if (destino.equals(TipoMoneda.GBP)) return cambiarEur(TipoMoneda.GBP, cambiarUsd(TipoMoneda.EUR, money));
-        else return -1;
+        else return -1f;
     }
 
     private static float cambiarGbp(TipoMoneda destino, float money){
         if (destino.equals(TipoMoneda.EUR)) return (money * cambioGbpEur);
         else if (destino.equals(TipoMoneda.USD)) return cambiarEur(TipoMoneda.USD, cambiarGbp(TipoMoneda.EUR, money));
-        else return -1;
+        else return -1f;
     }
 
     private static float cambiarEur(TipoMoneda destino, float money){
         if (destino.equals(TipoMoneda.USD)) return (money * cambioEurUsd);
         else if (destino.equals(TipoMoneda.GBP)) return (money * cambioEurGbp);
-        else return -1;
+        else return -1f;
     }
 
-    private static boolean comprobarSiMonedaEstaAceptada(TipoMoneda moneda){
+    private static boolean comprobarSiMonedaNoEstaAceptada(TipoMoneda moneda){
         for (TipoMoneda m : monedasAceptadas) {
-            if (m.equals(moneda)) return true;
+            if (m.equals(moneda)) return false;
         }
-        return false;
+        return true;
     }
 }
