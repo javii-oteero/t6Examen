c79c082d1997f943742e609f2e320af55e5a64a3
javi <javii.oteero@gmail.com>
2024-04-16 12:41:36 +0200

    Tests que deben dar fallo pasados correctamente

--- src/es/iesmz/tests/Money.java
+++ src/es/iesmz/tests/Money.java
@@ -2,7 +2,23 @@ package es.iesmz.tests;
 
 public class Money {
 
+    private final float cambioEurUsd = 1.18798f;
+    private final float cambioUsdEur = 0.841815f;
+    private final float cambioEurGbp = 0.857839f;
+    private final float cambioGbpEur = 1.165826f;
+
+    private static TipoMoneda[] monedasAceptadas = {TipoMoneda.USD, TipoMoneda.EUR, TipoMoneda.GBP};
+
     public static float change(TipoMoneda origen, TipoMoneda destino, float money){
+        if (money < 0) return -1f;
+        else if (!comprobarSiMonedaEstaAceptada(origen) || !comprobarSiMonedaEstaAceptada(destino)) return -1f;
         return 0f;
+    }
+
+    private static boolean comprobarSiMonedaEstaAceptada(TipoMoneda moneda){
+        for (TipoMoneda m : monedasAceptadas) {
+            if (m.equals(moneda)) return true;
+        }
+        return false;
     }
 }
