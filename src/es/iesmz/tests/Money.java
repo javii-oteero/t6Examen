8de02f18b69828cc2f14b50c90e86465c5216e39
javi <javii.oteero@gmail.com>
2024-04-16 13:11:31 +0200

    Metodos para cambiar divisas creados, todos los tests pasados

--- src/es/iesmz/tests/Money.java
+++ src/es/iesmz/tests/Money.java
@@ -2,17 +2,42 @@ package es.iesmz.tests;
 
 public class Money {
 
-    private final float cambioEurUsd = 1.18798f;
-    private final float cambioUsdEur = 0.841815f;
-    private final float cambioEurGbp = 0.857839f;
-    private final float cambioGbpEur = 1.165826f;
+    private static final float cambioEurUsd = 1.18798f;
+    private static final float cambioUsdEur = 0.841815f;
+    private static final float cambioEurGbp = 0.857839f;
+    private static final float cambioGbpEur = 1.165826f;
 
     private static TipoMoneda[] monedasAceptadas = {TipoMoneda.USD, TipoMoneda.EUR, TipoMoneda.GBP};
 
     public static float change(TipoMoneda origen, TipoMoneda destino, float money){
         if (money < 0) return -1f;
         else if (!comprobarSiMonedaEstaAceptada(origen) || !comprobarSiMonedaEstaAceptada(destino)) return -1f;
-        return 0f;
+        else {
+            switch (origen){
+                case GBP -> { return cambiarGbp(destino, money); }
+                case EUR -> { return cambiarEur(destino, money); }
+                case USD -> { return cambiarUsd(destino, money); }
+            }
+        }
+        return -1;
+    }
+
+    private static float cambiarUsd(TipoMoneda destino, float money){
+        if (destino.equals(TipoMoneda.EUR)) return (money * cambioUsdEur);
+        else if (destino.equals(TipoMoneda.GBP)) return cambiarEur(TipoMoneda.GBP, cambiarUsd(TipoMoneda.EUR, money));
+        else return -1;
+    }
+
+    private static float cambiarGbp(TipoMoneda destino, float money){
+        if (destino.equals(TipoMoneda.EUR)) return (money * cambioGbpEur);
+        else if (destino.equals(TipoMoneda.USD)) return cambiarEur(TipoMoneda.USD, cambiarGbp(TipoMoneda.EUR, money));
+        else return -1;
+    }
+
+    private static float cambiarEur(TipoMoneda destino, float money){
+        if (destino.equals(TipoMoneda.USD)) return (money * cambioEurUsd);
+        else if (destino.equals(TipoMoneda.GBP)) return (money * cambioEurGbp);
+        else return -1;
     }
 
     private static boolean comprobarSiMonedaEstaAceptada(TipoMoneda moneda){
--- src/es/iesmz/tests/MoneyTest.java
+++ src/es/iesmz/tests/MoneyTest.java
@@ -9,46 +9,46 @@ class MoneyTest {
 
     @Test
     void testEurUsd(){
-        assertEquals(Money.change(EUR, USD, 23.88f), 28.37f);
+        assertEquals(28.37f, Money.change(EUR, USD, 23.88f), 1);
     }
 
     @Test
     void testGbpEur(){
-        assertEquals(Money.change(GBP, EUR, 1000.0f), 1165.83f);
+        assertEquals(1165.83f, Money.change(GBP, EUR, 1000.0f), 1);
     }
 
     @Test
     void testEurGbp(){
-        assertEquals(Money.change(EUR, GBP, 234.56f), 201.21f);
+        assertEquals(201.21f, Money.change(EUR, GBP, 234.56f), 1);
     }
 
     @Test
     void testUsdEur(){
-        assertEquals(Money.change(USD, EUR, 44.56f), 37.51f);
+        assertEquals(37.51f, Money.change(USD, EUR, 44.56f), 1);
     }
 
     @Test
     void testGbpUsd(){
-        assertEquals(Money.change(GBP, USD, 100.0f), 138.49f);
+        assertEquals(138.49f, Money.change(GBP, USD, 100.0f), 1);
     }
 
     @Test
     void testUsdGbp(){
-        assertEquals(Money.change(USD, GBP, 1000.0f), 722.14f);
+        assertEquals(722.14f, Money.change(USD, GBP, 1000.0f), 1);
     }
 
     @Test
     void testFallidoPtsEur(){
-        assertEquals(Money.change(PTS, EUR, 100.0f), -1f);
+        assertEquals(-1f, Money.change(PTS, EUR, 100.0f));
     }
 
     @Test
     void testFallidoEurPts(){
-        assertEquals(Money.change(EUR, PTS, 123.3f), -1f);
+        assertEquals(-1f, Money.change(EUR, PTS, 123.3f));
     }
 
     @Test
     void testFallidoUsdEurNegativo(){
-        assertEquals(Money.change(USD, EUR, -167.34f), -1f);
+        assertEquals(-1f, Money.change(USD, EUR, -167.34f));
     }
 }
